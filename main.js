1d6ca2a6d64bcce93bc7cc2242524bfcb4e924f3
Pinja Alanne <pinja.alanne@gmail.com>
2023-10-11 14:10:56 +0300

    added a searchbar

--- main.js
+++ main.js
@@ -1,57 +1,66 @@
 const pokeContainer = document.querySelector('.pokecontainer')
+const input = document.querySelector('#search')
 
 let pokeData = []
 
-const fetchData = async () => { await
+const fetchData = () => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=121&offset=0')
-    .then((res) => res.json())
-    .then((data) => {
-        const fetches = data.results.map((item) => {
-            return fetch(item.url)
-            .then((res) => res.json())
-            .then((data) => {
-                return {
-                    id: data.id,
-                    name: data.name,
-                    img: data.sprites.other['official-artwork'].front_default,
-                    types: data.types,
-                    height: data.height,
-                    weight: data.weight
-                };
+        .then((res) => res.json())
+        .then((data) => {
+            const fetches = data.results.map((item) => {
+                return fetch(item.url)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        return {
+                            id: data.id,
+                            name: data.name,
+                            img: data.sprites.other['official-artwork'].front_default,
+                            types: data.types,
+                            height: data.height,
+                            weight: data.weight
+                        };
+                    });
+            });
+            Promise.all(fetches).then((res) => {
+                pokeData = res;
+                pokeMons();
             });
         });
-        Promise.all(fetches).then((res) => {
-            pokeData = res;
-            pokeMons();
-            console.log(pokeData);
-    });
-    });
 }
 
-const pokeMons = () => {
-   const cards = pokeData.map(pokemon => {
-    console.log(pokemon.types);
-    return  `<div class="pokebox">
-           <p class="number">#${pokemon.id}</p>
-           <div class="pokeboximg">
-             <img class="bulba" src="${pokemon.img}" alt="no img" />
-           </div>
-           <div class="poketext">
-             <h2 class="pokeboxhead">${pokemon.name}</h2>
-             <p class="pokeboxtext">Height: ${pokemon.height} Weight: ${pokemon.weight}</p>
-             <div class="types">
-            ${pokemon.types.map((type) => getType(type)).join('|')}</p>
-             </div>
-           </div>
-         </div>`
-   }
-   ).join('')
+const pokeMons = (filteredData = pokeData) => {
+    const cards = filteredData.map(pokemon => {
+        return  `<div class="pokebox">
+               <p class="number">#${pokemon.id}</p>
+               <div class="pokeboximg">
+                 <img class="bulba" src="${pokemon.img}" alt="no img" />
+               </div>
+               <div class="poketext">
+                 <h2 class="pokeboxhead">${pokemon.name}</h2>
+                 <p class="pokeboxtext">Height: ${pokemon.height} Weight: ${pokemon.weight}</p>
+                 <div class="types">
+                ${pokemon.types.map((type) => type.type.name).join('|')}
+                 </div>
+               </div>
+             </div>`;
+    }).join('');
 
-        pokeContainer.innerHTML = cards;
+    pokeContainer.innerHTML = cards;
 }
 
-const getType = (type) => {
-    return `<p>${type.type.name}</p>`
+const searchPokes = () => {
+    input.addEventListener("keyup", () => {
+        const searchString = input.value.toLowerCase();
+        if (searchString) {
+            const filteredPokemons = pokeData.filter(pokemon => {
+                return pokemon.name.toLowerCase().includes(searchString);
+            });
+            pokeMons(filteredPokemons);
+        } else {
+            pokeMons(pokeData);
+        }
+    });
 }
 
 fetchData();
+searchPokes();
